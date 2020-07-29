dcfb5a4e7a7f2e6f30c7927ab79a9bf41c8f0cee
Ava Pek <exodrifter@gmail.com>
2020-07-28 23:37:47 -0500

    Added the SoundbankHitPlayer component
    This component can be used to play one-off sounds from a soundbank,
    similar to what the `AudioClipHitPlayer` component does.

---
+++ Scripts/Components/SoundbankHitPlayer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Exodrifter.Aural
+{
+	public class SoundbankHitPlayer : AudioPlayer
+	{
+		public Soundbank Soundbank { get { return soundbank; } }
+		[SerializeField]
+		private Soundbank soundbank = null;
+
+		private void Start()
+		{
+			Audio.Hit(soundbank, param, transform.position);
+		}
+	}
+}
